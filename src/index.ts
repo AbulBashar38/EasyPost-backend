72f64d2017096b06cb07bf5a47e82aea47b07a42
basar <abulbasarofficial5403@gmail.com>
2026-02-09 13:35:21 +0600

    port change

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use(router);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
